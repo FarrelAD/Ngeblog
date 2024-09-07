30976b63d87ba864043e60b2a8a951aff632d7c8
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-09-07 13:02:32 +0700

    add new subcomponent for blogs topic and add its content preview

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,16 +1,11 @@
 import { useState } from "react"
+import PropTypes from 'prop-types'
 
 import Card from "./Card"
 import Footer from "./Footer"
 
-function LandingPage() {
-    const [activeMenuCard, setActiveMenuCard] = useState(null)
-
-    const toggleDetail = (id) => {
-        setActiveMenuCard(id)
-    }
-
-    const blogContents = [
+const blogContents = [
+    [
         {
             blogTitle: 'Cara menjadi programmer handal yang berguna memajukan UMKM masyarakat Indonesia',
             blogWriter: 'Tejo Sudirman',
@@ -52,26 +47,72 @@ function LandingPage() {
             writerPhotoProfile: 'https://img.freepik.com/free-photo/front-view-man-looking-up-with-copy-space_23-2148946263.jpg?t=st=1725161263~exp=1725164863~hmac=73d0f40c0ce9144b9dae670de191c9291ed3ae40630067eeaf81cadc30e8796a&w=1060',
             blogImageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7FW_yu-b_ahRRx6zWNt9weK3tlypMIoGMim5kZQfdJFtsh7A_4YBn7v6U5nhjOveiSbw&usqp=CAU',
             blogWrittenDate: '05 Agt 2024'
+        }
+    ],
+    [
+        {
+            blogTitle: 'Rekomendasi Novel Bergenre Horor Edisi Tahun 2024',
+            blogWriter: 'Gunawan Sulityo',
+            writerPhotoProfile: 'https://img.freepik.com/free-photo/handsome-young-man-with-arms-crossed-white-background_23-2148222620.jpg?t=st=1725687507~exp=1725691107~hmac=7dd148a677dcea53909532bc9cd77cb2ccc44d83d7acace7cd13e54bc4da1997&w=1060',
+            blogImageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE_PHTZrzGex_snVnMN61z6DTv8-ZnD1ng7Q&s',
+            blogWrittenDate: '07 Jan 2024'
         },
+        {
+            blogTitle: 'Cara Custom Headlamp Motor Vario 160 Terbaru',
+            blogWriter: 'Anjar Menawan',
+            writerPhotoProfile: 'https://img.freepik.com/free-photo/front-view-man-pointing-up_23-2148946191.jpg?t=st=1725687734~exp=1725691334~hmac=ffd3d2221026b7b540066a790ceeda44edcb5888e0a6b947d30586a82e97dd6a&w=1060',
+            blogImageUrl: 'https://imgx.gridoto.com/crop/0x0:0x0/700x465/photo/2022/06/23/modifikasi-honda-vario-160jpg-20220623013734.jpg',
+            blogWrittenDate: '29 Mar 2024'
+        }
+    ],
+    [
+        {
+            blogTitle: 'Caraku Mencoba Berhemat untuk Kebutuhan Sehari-Hari sebagai Anak Kost',
+            blogWriter: '-',
+            writerPhotoProfile: 'https://i.pinimg.com/736x/09/21/fc/0921fc87aa989330b8d403014bf4f340.jpg',
+            blogImageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyvoiazVTZLE9OgYJ3VGPz2-_rk1UM1doQFA&s',
+            blogWrittenDate: '16 Agt 2024'
+        },
+        {
+            blogTitle: 'Tips Biar Bikin Lo PEDE Ngomong di Depan Umum!',
+            blogWriter: 'Agung Hermawan',
+            writerPhotoProfile: 'https://i.pinimg.com/736x/09/21/fc/0921fc87aa989330b8d403014bf4f340.jpg',
+            blogImageUrl: 'https://static.promediateknologi.id/crop/0x0:0x0/0x0/webp/photo/jawapos/2019/09/orang-bicara.jpg',
+            blogWrittenDate: '19 Jun 2024'
+        }
+    ],
+    [
+        {
+            blogTitle: 'Cara Mempersiapkan Lamaran Pekerjaan untuk Kerja Remote di Luar Negeri sebagai Software Engineer',
+            blogWriter: 'Agung Hermawan',
+            writerPhotoProfile: 'https://i.pinimg.com/736x/09/21/fc/0921fc87aa989330b8d403014bf4f340.jpg',
+            blogImageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTB2qyef9KGRV-TdNxRNF20EErY5uyBfkh71A&s-',
+            blogWrittenDate: '26 Feb 2024'
+        },
+        {
+            blogTitle: 'Panduan Karir bagi Generasi Milenial',
+            blogWriter: 'Angel Feronica',
+            writerPhotoProfile: 'https://i.pinimg.com/736x/09/21/fc/0921fc87aa989330b8d403014bf4f340.jpg',
+            blogImageUrl: 'https://www.hadirr.com/blog/wp-content/uploads/2022/10/Cara-Tepat-Mengelola-Karyawan-Milenial.jpg',
+            blogWrittenDate: '18 Mar 2024'
+        },
+        {
+            blogTitle: 'Pengalaman Berwirausaha Ternak Lele bareng Kawan SD',
+            blogWriter: 'Ahmad Gunawan',
+            writerPhotoProfile: 'https://i.pinimg.com/736x/09/21/fc/0921fc87aa989330b8d403014bf4f340.jpg',
+            blogImageUrl: 'https://static.promediateknologi.id/crop/0x0:0x0/0x0/webp/photo/p2/132/2024/01/27/cara-ternak-lele-1942238737.jpg',
+            blogWrittenDate: '04 Agt 2024'
+        }
     ]
+]
 
-    return (
-        <div className="p-20 pb-4">
-            <div className="flex justify-between align-middle">
-                <div className="flex flex-col justify-center ml-16">
-                    <h1 className="font-bold text-7xl">NGEBLOG.</h1>
-                    <p>Mari saling berbagi ide, membantu satu sama lain, dan berikan pengalamanmu semuanya di sini!</p>
-                </div>
-
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-20 h-80 w-80 mr-32">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
-                </svg>
-            </div>
-
-            <section>
-                <h1 className="- font-bold text-3xl">Teknologi</h1>
+function BlogsTopicContainer({topicName, topicIndex, activeMenuCard, toggleDetail}) {
+    return(
+        <>
+            <section className="my-4">
+                <h1 className="- font-bold text-3xl">{topicName}</h1>
                 <div id="card-container" className="grid grid-cols-[repeat(auto-fill,_22rem)] gap-1 auto-rows-[24rem] mt-4">
-                    {blogContents.map((content, index) => (
+                    {blogContents[topicIndex].map((content, index) => (
                         <Card 
                             key={index}
                             id={index}
@@ -86,6 +127,64 @@ function LandingPage() {
                     ))}
                 </div>
             </section>
+        </>
+    )
+}
+
+BlogsTopicContainer.propTypes = {
+    topicName: PropTypes.string.isRequired,
+    topicIndex: PropTypes.number.isRequired,
+    activeMenuCard: PropTypes.number.isRequired,
+    toggleDetail: PropTypes.func.isRequired,
+}
+
+function LandingPage() {
+    const [activeMenuCard, setActiveMenuCard] = useState(null)
+
+    const toggleDetail = (id) => {
+        setActiveMenuCard(id)
+    }
+
+    return (
+        <div className="p-20 pb-4">
+            <div className="flex justify-between align-middle">
+                <div className="flex flex-col justify-center ml-16">
+                    <h1 className="font-bold text-7xl">NGEBLOG.</h1>
+                    <p>Mari saling berbagi ide, membantu satu sama lain, dan berikan pengalamanmu semuanya di sini!</p>
+                </div>
+
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-20 h-80 w-80 mr-32">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
+                </svg>
+            </div>
+
+            <BlogsTopicContainer
+                topicName="Teknologi"
+                topicIndex={0}
+                activeMenuCard={activeMenuCard}
+                toggleDetail={toggleDetail}
+            />
+
+            <BlogsTopicContainer
+                topicName="Hobi dan Minat"
+                topicIndex={1}
+                activeMenuCard={activeMenuCard}
+                toggleDetail={toggleDetail}
+            />
+
+            <BlogsTopicContainer
+                topicName="Pengembangan Diri"
+                topicIndex={2}
+                activeMenuCard={activeMenuCard}
+                toggleDetail={toggleDetail}
+            />
+
+            <BlogsTopicContainer
+                topicName="Karir"
+                topicIndex={3}
+                activeMenuCard={activeMenuCard}
+                toggleDetail={toggleDetail}
+            />
 
             <Footer />
         </div>
