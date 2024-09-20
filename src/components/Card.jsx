6d38fa979a41470fb281b17c8186344b27592e1f
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-09-20 23:06:19 +0700

    make the web page responsive - homepage

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -35,7 +35,7 @@ function Card({id, isMenuActive, toggleDetail, blogTitle, blogWriter, writerPhot
     }
 
     return (
-        <div className="blog-card bg-slate-950 rounded-lg w-80 m-4 shadow-lg shadow-slate-600 hover:cursor-pointer hover:shadow-slate-400 transition-shadow" onClick={navigateDetail}>
+        <div className="mx-auto blog-card bg-slate-950 rounded-lg w-80 m-4 shadow-lg shadow-slate-600 hover:cursor-pointer hover:shadow-slate-400 transition-shadow" onClick={navigateDetail}>
             <img src={blogImageUrl} alt="Card image" className="min-w-full h-48 rounded-t-lg object-cover" />
             <div className="p-4">
                 <h1 className="blog-title font-bold">{blogTitle}</h1>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,9 +9,9 @@ import { blogContents } from "../seed/data"
 function BlogsTopicContainer({topicName, topicIndex, activeMenuCard, toggleDetail}) {
     return(
         <>
-            <section className="my-4">
-                <h1 className="- font-bold text-3xl">{topicName}</h1>
-                <div id="card-container" className="grid grid-cols-[repeat(auto-fill,_22rem)] gap-1 auto-rows-[24rem] mt-4">
+            <section className="my-16 md:my-4">
+                <h1 className="font-bold text-3xl text-center md:text-left">{topicName}</h1>
+                <div id="card-container" className="grid grid-cols-1 md:grid-cols-[repeat(auto-fill,_22rem)] gap-1 auto-rows-[24rem] mt-4">
                     {blogContents[topicIndex].map((content, index) => (
                         <Card 
                             key={index}
@@ -46,16 +46,15 @@ function HomePage() {
     }
 
     return (
-        <div className="p-20 pb-4">
-            <div className="flex justify-between align-middle">
-                <div className="flex flex-col justify-center ml-16">
-                    <h1 className="font-bold text-7xl">NGEBLOG.</h1>
-                    <p>Mari saling berbagi ide, membantu satu sama lain, dan berikan pengalamanmu semuanya di sini!</p>
-                </div>
-
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-20 h-80 w-80 mr-32">
+        <div className="p-4 md:p-20 pb-4">
+            <div className="md:flex md:justify-between md:align-middle">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-11 md:size-20 h-40 md:h-80 w-40 md:w-80 md:mr-32 order-1 md:order-2 mx-auto">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
                 </svg>
+                <div className="flex flex-col justify-center mx-auto p-8 md:ml-16 order-2 md:order-1">
+                    <h1 className="font-bold text-3xl md:text-7xl">NGEBLOG.</h1>
+                    <p>Mari saling berbagi ide, membantu satu sama lain, dan berikan pengalamanmu semuanya di sini!</p>
+                </div>
             </div>
 
             <BlogsTopicContainer
